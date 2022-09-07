128ac4a217813602ea2c369408ec4a9a18a192bd
4eDo <61490588+4eDo@users.noreply.github.com>
2022-09-07 11:03:35 +0300

    Update demo.user.js

--- Grind/demo.user.js
+++ Grind/demo.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Гринд
-// @version      0.9
+// @version      0.10
 // @description  Гринд...
 // @namespace    http://tampermonkey.net/
 // @author       4eDo (https://github.com/4eDo/lowadi)
@@ -10,7 +10,7 @@
 	
 
     if(location.href.indexOf('elevage/bureau/') !== -1) {
-		$('section.content__middle > ul.tab').append('<li id="tab-grind-settings" class="tab-style-6-0-0"><div><a style="" href="https://www.lowadi.com/elevage/bureau/#tab-grind-settings" class="tab-action">Настройки скрипта</a></div></li>');
+		$('section.content__middle > ul.tab').append('<li id="tab-grind-settings" class="tab-style-6-0-0"><div><a style="" href="' + location.href + '#tab-grind-settings" class="tab-action">Настройки скрипта</a></div></li>');
 		fetchGrindSettingsFetch();
 		$('#tab-grind-settings').click(function(){showGrindSettings();});
 	}
